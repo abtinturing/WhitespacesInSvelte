3b7e52336f355dce0d7945ef04d26800db7b7855
abtinturing <abtinma@yahoo.com>
2024-08-07 21:40:28 -0400

    simplified the component

--- src/lib/MyComponent.svelte
+++ src/lib/MyComponent.svelte
@@ -2,9 +2,5 @@
 	const data = { content: 'hello', lang: 'js' };
 </script>
 
-<div>
-	{#if data.lang}
-		<span class="lang">{data.lang}</span>
-	{/if}
-</div>
+<div>{data.lang}</div>
 <div>{@html data.content}</div>
