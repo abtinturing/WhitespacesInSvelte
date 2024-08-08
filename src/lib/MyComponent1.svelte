079e4ba03b8278c0286e613b010ac62a408c2b58
abtinturing <abtinma@yahoo.com>
2024-08-08 02:21:19 -0400

    Fixed component 1

--- src/lib/MyComponent1.svelte
+++ src/lib/MyComponent1.svelte
@@ -1,18 +1,6 @@
 <script>
-	const data = { content: 'hello', lang: 'js', start: '```', end: '```' };
+	const data = { content: 'hello', lang: 'js' };
 </script>
 
-{#if data}
-	<div class="code-block" spellcheck="false">
-		<div class="line">
-			<span class="symbol">{data.start}</span>{#if data.lang}<span class="lang">{data.lang}</span
-				>{/if}
-		</div>
-		{#if data.content}{#each data.content.split('\n') as line}<div class="line">
-					{@html line}
-				</div>{/each}{/if}{#if data.end}<div class="line">
-				<span class="symbol">{data.end}</span>
-			</div>
-		{/if}
-	</div>
-{/if}
+<div>{data.lang}</div>
+<div>{@html data.content}</div>
