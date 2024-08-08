52b39397bc32332800da7a99398359f5d4b17826
abtinturing <abtinma@yahoo.com>
2024-08-07 21:32:09 -0400

    First commit

---
+++ src/lib/MyComponent.svelte
@@ -0,0 +1,10 @@
+<script>
+	const data = { content: 'hello', lang: 'js' };
+</script>
+
+<div>
+	{#if data.lang}
+		<span class="lang">{data.lang}</span>
+	{/if}
+</div>
+<div>{@html data.content}</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import MyComponent from '$lib/MyComponent.svelte';
+	import { mount, onMount } from 'svelte';
+
+	onMount(() => {
+		const container = document.createElement('div');
+		mount(MyComponent, { target: container });
+
+		console.warn({ innerText: container.innerText, container, innerHTML: container.innerHTML });
+	});
+</script>
